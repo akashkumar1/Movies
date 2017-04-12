41265fa43d11b798d4621a7b38bf9d6d084b3988
akashkumar1 <akash12011@iiitd.ac.in>
2017-04-13 02:29:05 +0530

    model created & locations fetched

--- movies/web/models.py
+++ movies/web/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+	title=models.CharField(max_length=100)
+	year=models.IntegerField()
+	lat=models.FloatField()
+	lon=models.FloatField()
+	writer=models.CharField(max_length=100)
+	production=models.CharField(max_length=100)
+	director=models.CharField(max_length=100)
+	actor1=models.CharField(max_length=100)
+	actor2=models.CharField(max_length=100)
+	actor3=models.CharField(max_length=100)
+
--- movies/web/views.py
+++ movies/web/views.py
@@ -3,4 +3,31 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+import json
+import requests
+import traceback
+from django.http import HttpResponse
+
+from geopy.geocoders import Nominatim
 # Create your views here.
+
+def api(request):
+	query=request.GET.get('q','')
+	data_url='http://data.sfgov.org/resource/wwmu-gmzc.json'
+	req=requests.get(data_url,verify=True)
+	data=json.loads(req.text)
+
+	for row in data:
+		try:
+			
+			try:
+				place=(row['locations']).encode('utf-8')
+				geolocator = Nominatim()
+				location = geolocator.geocode(str(place))
+				lat=location.latitude
+				lon=location.longitude
+				print lat,lon
+			except:
+				print traceback.print_exc()	
+		except:
+			print traceback.print_exc()
